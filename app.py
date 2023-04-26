ec36eb29258f4b6683639351296a75b9d0bd4b59
Waifumorena <131816799+Waifumorena@users.noreply.github.com>
2023-04-26 10:27:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
 import threading
 import asyncio
 import websockets
-import mysql.connector
+import sqlite3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Conexión a la base de datos MySQL
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="waifusmo@localhost",
-  password="Yasuo123-",
-  database="waifusmo_Emy"
-)
+# Conexión a la base de datos SQLite
+conn = sqlite3.connect('waifusmo_Emy.db')
+c = conn.cursor()
+
+# Crear la tabla mensajes si no existe
+c.execute('''CREATE TABLE IF NOT EXISTS mensajes
+             (mensaje text)''')
 
 @app.route('/')
 def home():
@@ -35,7 +34,7 @@ def login():
 
 
 # Dirección IP del servidor
-HOST = '127.0.0.1'
+HOST = '149.56.67.133'
 # Puerto en el que se ejecutará el servidor
 PORT = 5001
 
@@ -52,12 +51,9 @@ async def manejar_cliente(websocket, path):
         for cliente in clientes:
             await cliente.send(mensaje)
 
-        # Guardar el mensaje en la base de datos MySQL
-        cursor = mydb.cursor()
-        sql = "INSERT INTO mensajes (mensaje) VALUES (%s)"
-        val = (mensaje,)
-        cursor.execute(sql, val)
-        mydb.commit()
+        # Guardar el mensaje en la base de datos SQLite
+        c.execute("INSERT INTO mensajes (mensaje) VALUES (?)", (mensaje,))
+        conn.commit()
 
     # Eliminar el cliente de la lista de clientes
     clientes.remove(websocket)
@@ -70,7 +66,7 @@ async def iniciar_servidor():
 
 # Iniciar el servidor Flask y el servidor WebSocket en hilos separados
 if __name__ == '__main__':
-    hilo_flask = threading.Thread(target=app.run)
+    hilo_flask = threading.Thread(target=app.run(host='149.56.67.133', port=5000))
     hilo_websockets = threading.Thread(target=asyncio.run, args=(iniciar_servidor(),))
 
     hilo_flask.start()
@@ -78,6 +74,10 @@ if __name__ == '__main__':
 
     hilo_flask.join()
     hilo_websockets.join()
+
+    # Cerrar la conexión a la base de datos SQLite
+    conn.close()
+
 
 
 
