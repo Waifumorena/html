cc76985edbf8359e40f7cd3e3a71c2290db44e40
Waifumorena <131816799+Waifumorena@users.noreply.github.com>
2023-04-26 09:23:20 -0700

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -1,1 +1,16 @@
+// Conectarse al servidor utilizando WebSocket
+var socket = new WebSocket('ws://127.0.0.1:5001/');
 
+// Manejar la recepción de mensajes del servidor
+socket.onmessage = function(event) {
+  var mensaje = event.data;
+  $('#mensajes').append('<div>' + mensaje + '</div>');
+}
+
+// Manejar el envío de mensajes desde el formulario de entrada
+$('#formulario').submit(function(event) {
+  event.preventDefault();
+  var mensaje = $('#mensaje').val();
+  socket.send(mensaje);
+  $('#mensaje').val('');
+});
